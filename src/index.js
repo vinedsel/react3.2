409a60ba267641ec363f8cf1cad1fb2d6f10a999
Raymond Gregory <vinedsel@gmail.com>
2018-12-19 16:14:53 -0600

    Created code to match project specifics, using the previous project (react3) as a template.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = props => {
+  const favoriteCars = ['Honda CR-V ', 'Toyota Highlander', 'Ford F-150', 'BMW X3', 'Subaru Crosstrek']
+
+  return (
+    <div>
+      <List favoriteCars={favoriteCars} />
+    </div>
+  );
+};
+
+const List = props => {
+  const listCars = props.favoriteCars.map(
+    (favoriteCars, index) => (
+    <li key={index}>
+      {favoriteCars}
+    </li>
+  ));
+
+  return <ol>{listCars}</ol>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
